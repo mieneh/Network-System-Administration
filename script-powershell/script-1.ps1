258d5cf873f427c00f08d913be13481ef0859255
mieneh <dinhphuongmy21012003@gmail.com>
2024-06-14 00:22:56 +0700

    Update

---
+++ script-powershell/script-1.ps1
@@ -0,0 +1,65 @@
+﻿# CAU HINH DIA CHI IP CHO FIT-DC
+$adapter = "Ethernet0"
+$ipaddress = "192.168.1.2"
+$subnetmask = "255.255.255.0"
+$defaultgateway = "192.168.1.1"
+
+New-NetIPAddress -InterfaceAlias $adapter -IPAddress $ipaddress -PrefixLength 24 -DefaultGateway $defaultgateway
+
+Set-DnsClientServerAddress -InterfaceAlias $adapter -ServerAddresses $ipaddress
+
+# CAU HINH DOMAIN CONTROLLER
+# Cai dat AD DS
+Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
+
+# Tao forest moi
+Install-ADDSForest -DomainName "labtdtu.com" -InstallDns -Force:$true
+
+# Kiem tra DC
+Get-ADDomainController -Filter * | Select Name, Domain, IsGlobalCatalog, OperationMasterRoles
+
+
+# Doi ten may server
+Rename-Computer -NewName "FIT-DC" -Restart
+
+# Cau hinh lai DNS
+Set-DnsClientServerAddress -InterfaceAlias "Ethernet0" -ServerAddresses "192.168.1.2"
+
+# Cau hinh ban ghi cho www.labtdtu.com
+$webServerIP = "192.168.1.3"
+$recordName = "www"
+$zoneName = "labtdtu.com"
+$dnsServer = "FIT-DC"
+
+Add-DnsServerResourceRecordA -Name $recordName -ZoneName $zoneName -IPv4Address $webServerIP -ComputerName $dnsServer
+
+Get-DnsServerResourceRecord -ZoneName "labtdtu.com" -Name "www" -RRType A -ComputerName "FIT-DC"
+
+# -> CHUYEN QUA FIT-WEB
+
+# CAI DAT AD CS
+Install-WindowsFeature -Name AD-Certificate -IncludeManagementTools
+Install-WindowsFeature -Name ADCS-Cert-Authority -IncludeManagementTools
+Install-WindowsFeature -Name ADCS-Web-Enrollment 
+
+# Cau hinh AD CS
+Install-ADcsCertificationAuthority `
+    –Credential (Get-Credential) `
+    -CAType EnterpriseRootCA `
+    –CACommonName “LABTDTU-CA” `
+    –CADistinguishedNameSuffix “DC=labtdtu,DC=com” `
+    –CryptoProviderName “RSA#Microsoft Software Key Storage Provider” `
+    -KeyLength 2048 `
+    –HashAlgorithmName SHA1 `
+    –ValidityPeriod Years `
+    –ValidityPeriodUnits 3 `
+    –DatabaseDirectory “C:\windows\system32\certLog” `
+    –LogDirectory “c:\windows\system32\CertLog” `
+    –Force
+
+Install-AdcsWebEnrollment
+
+Install-WindowsFeature -Name ADCS-Online-Cert
+Install-AdcsOnlineResponder
+
+# -> CHUYEN QUA FIT-WEB
---
+++ script-powershell/script-2.ps1
@@ -0,0 +1,70 @@
+﻿# CAU HINH DIA CHI IP CHO FIT-WEB
+$adapter = "Ethernet0"
+$ipaddress = "192.168.1.3"
+$subnetmask = "255.255.255.0"
+$defaultgateway = "192.168.1.1"
+$dnsaddress = "192.168.1.2"
+
+New-NetIPAddress -InterfaceAlias $adapter -IPAddress $ipaddress -PrefixLength 24 -DefaultGateway $defaultgateway
+
+Set-DnsClientServerAddress -InterfaceAlias $adapter -ServerAddresses $dnsaddress
+
+# Doi ten may server
+Rename-Computer -NewName "FIT-WEB" -Restart
+
+# Join vao domain labtdtu.com
+Add-Computer -DomainName "labtdtu.com" -Credential (Get-Credential) -Restart
+
+# Cau hinh Web Server
+Install-WindowsFeature -Name Web-Server -IncludeManagementTools
+
+# -> CHUYEN QUA FIT-DC CAI DAT AD CS
+
+# Tao Certificate Request
+
+$CertName = "www.labtdtu.com"
+$CSRPath = "C:\labtdtu.csr"
+$INFPath = "C:\labtdtu.inf"
+
+$INF =
+@"
+    [Version]
+
+    [NewRequest]
+    Subject = "CN=$CertName, OU=TDTU, O=TDTU, L=TDTU, S=TDTUs, C=US"
+    KeySpec = 1
+    KeyLength = 2048
+    Exportable = TRUE
+    MachineKeySet = TRUE
+    SMIME = False
+    PrivateKeyArchive = FALSE
+    UserProtected = FALSE
+    UseExistingKeySet = FALSE
+    ProviderName = "Microsoft RSA SChannel Cryptographic Provider"
+    ProviderType = 12
+    RequestType = PKCS10
+    KeyUsage = 0xa0
+    [EnhancedKeyUsageExtension]
+    OID=1.3.6.1.5.5.7.3.1 
+"@
+
+$INF | Out-File -FilePath $INFPath -Force
+certreq.exe -new $INFPath $CSRPath
+
+# -> CHUYEN QUA IE DE TIEN HANH TAI FILE CER
+
+$CertPath = "C:\Users\Administrator\AppData\Local\Microsoft\Windows\INetCache\IE\3S18VYG4\certnew.cer"
+
+Import-Certificate -FilePath $CertPath -CertStoreLocation "Cert:\LocalMachine\My"
+
+# Binding https cho default web site
+
+$WebsiteName = "Default Web Site"
+
+$Site = Get-Website -Name $WebsiteName
+
+New-WebBinding -Name $Site.Name -IPAddress "*" -Port 443 -Protocol "https"
+
+$Cert = Get-ChildItem -Path Cert:\LocalMachine\My | Where-Object { $_.Subject -like "*www.labtdtu.com*" }
+$Binding = Get-WebBinding -Name $Site.Name -Port 443
+$Binding.AddSslCertificate($Cert.GetCertHashString(), "My")
